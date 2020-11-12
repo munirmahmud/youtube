4b4fbee4d3d8c490ef5da169f2e461f7520f295a
Munir Mahmud <munirmahmud6@gmail.com>
2020-11-12 20:26:25 +0600

    Recommended video component added

---
+++ src/components/Videos/RecommendedVideos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RecomendedVideos.css';
+
+const RecommendedVideos = () => {
+    return (
+        <main className="recommendedVideos">
+            Recommended Videos
+        </main>
+    )
+}
+
+export default RecommendedVideos
