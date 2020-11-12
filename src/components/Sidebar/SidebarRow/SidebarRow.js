8e676e29a7fec2e0ce11099225f4971fa0ebcc57
Munir Mahmud <munirmahmud6@gmail.com>
2020-11-12 20:20:12 +0600

    Sidebar Row component added

---
+++ src/components/Sidebar/SidebarRow/SidebarRow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SidebarRow.css';
+
+const SidebarRow = ({ title }) => {
+    return (
+        <div>
+            {title}
+        </div>
+    );
+};
+
+export default SidebarRow;
